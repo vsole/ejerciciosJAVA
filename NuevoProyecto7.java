58fe21c3ef7d6b894b876e61dbdc9a8b6d6537de
vsole <31908760+vsole@users.noreply.github.com>
2017-09-13 17:46:59 -0300

    Commit 7

---
+++ NuevoProyecto7.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevo.proyecto7;
+
+/**
+ *
+ * @author alumno
+ */
+public class NuevoProyecto7 {
+
+    /**
+     * @param args the command line arguments
+     * Se pide la base de un cuadrado y se informa el perimetro.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
