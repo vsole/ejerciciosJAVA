97d9e52d1e2c74bca06ed42fd28489efc7ac7ff8
vsole <31908760+vsole@users.noreply.github.com>
2017-09-13 17:49:26 -0300

    Commit 8

---
+++ NuevoProyecto8.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevo.proyecto8;
+
+/**
+ *
+ * @author alumno
+ */
+public class NuevoProyecto8 {
+
+    /**
+     * @param args the command line arguments
+     * Se pide el largo y el ancho de un terreno y se informa la 
+     * superficie del mismo.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
