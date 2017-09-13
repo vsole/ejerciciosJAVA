c3a07542dc4d8a46dea1ae06e08c4995a35311e2
vsole <31908760+vsole@users.noreply.github.com>
2017-09-13 17:44:36 -0300

    Commit4

---
+++ NuevoProyecto4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevo.proyecto4;
+
+/**
+ *
+ * @author alumno
+ */
+public class NuevoProyecto4 {
+
+    /**
+     * @param args the command line arguments
+     * Al ingresar una edad y el nombre se debe mostarr el siguiente mesaje
+       Usted se llama xxx y tiene xxx años de edad     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
