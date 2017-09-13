06c99f505c493efd2b9b6ab6295aeab05e29ce60
vsole <31908760+vsole@users.noreply.github.com>
2017-09-13 17:45:12 -0300

    Commit 5

---
+++ NuevoProyecto5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevo.proyecto5;
+
+/**
+ *
+ * @author alumno
+ */
+public class NuevoProyecto5 {
+
+    /**
+     * @param args the command line arguments
+     * Se necesita pedir 2 numeros y sumarlos, mostrar el resultado.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
