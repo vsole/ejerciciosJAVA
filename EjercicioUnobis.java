7b7610da0fdd990cff2fb376ede4627c5ac85a32
vsole <31908760+vsole@users.noreply.github.com>
2017-09-14 00:12:42 -0300

    Add files via upload
    Modifique un error. habia escrito el codigo en donde no correspondia

---
+++ EjercicioUnobis.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Valeria
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     * Se debe mostrar el siguiente mensaje:
+     * Esto funciona de maravilla
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Esto funciona de maravilla");
+    }
+    
+}
